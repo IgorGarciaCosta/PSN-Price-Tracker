8d4d4007e7ee020e5b15d71468d9a7ef986261aa
IgorGarciaCosta <igonildo7@gmail.com>
2026-04-06 01:14:58 -0300

    Add Dockerfile, docker-compose and .dockerignore

--- obj/Debug/net9.0/PsnPriceTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/PsnPriceTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PsnPriceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1baa79a72a6f054de7c2bb88cea0a2b948890ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e80214f7a72e33af43f6a0566479c93f18c103")]
 [assembly: System.Reflection.AssemblyProductAttribute("PsnPriceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PsnPriceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
